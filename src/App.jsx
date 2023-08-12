06ad6b1c3988495f1609d51ecfcd3c65e9276cb8
Sanjar <sanjarjangabaev6@gmail.com>
2023-08-12 15:32:02 +0500

    circle-bar

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useRef, useState, useEffect } from 'react';
+import CircleBar from "./CircleBar.jsx";
+import './App.css';
+
+const App = () => {
+    const [percentage, setPercentage] = useState(0);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const bar = ref.current;
+        const style = getComputedStyle(bar).strokeDasharray;
+        const styleNow = parseFloat(style) * (1 - parseFloat(percentage / 100));
+        bar.style.strokeDashoffset = styleNow;
+    }, [percentage]);
+
+    return (
+        <div className={'bar'}>
+            <h1>Homework (useState, useEffect, useRef)</h1>
+            <div className={'bar-block'}>
+                <div className={'card'} ref={ref} onScroll={(event)=>setPercentage(Math.floor(event.target.scrollTop *100/500))}>
+                    <div className={'card-item'}>
+                        <p>$</p>
+                        <p>#</p>
+                        <p>!</p>
+                    </div>
+                    <CircleBar percentage={percentage} circleWidth='200'/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/CircleBar.jsx
@@ -0,0 +1,33 @@
+
+const CircleBar = ({ percentage, circleWidth }) => {
+    const radius = 85;
+    const dashArray = radius * Math.PI * 2;
+    const dashOffset = dashArray - (dashArray * percentage) / 100;
+    const rotation = -90; // Rotation angle in degrees
+
+    return (
+        <div className={'circle'}>
+            <svg width={circleWidth} height={circleWidth} viewBox={`0 0 ${circleWidth} ${circleWidth}`}>
+                <circle cx={circleWidth / 2} cy={circleWidth / 2} strokeWidth={'15px'} r={radius} className={'circle-background'} />
+                <circle
+                    cx={circleWidth / 2}
+                    cy={circleWidth / 2}
+                    strokeWidth={'15px'}
+                    r={radius}
+                    className={'circle-progress'}
+                    style={{
+                        strokeDasharray: dashArray,
+                        strokeDashoffset: dashOffset,
+                        transform: `rotate(${rotation}deg)`,
+                        transformOrigin: `${circleWidth / 2}px ${circleWidth / 2}px`,
+                    }}
+                />
+                <text x={'50%'} y={'50%'} dy={'0.3em'} textAnchor={'middle'}>
+                    {percentage}%
+                </text>
+            </svg>
+        </div>
+    );
+};
+
+export default CircleBar;
